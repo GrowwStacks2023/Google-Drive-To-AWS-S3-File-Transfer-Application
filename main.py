d49382eac92afd69b89a146c58fdfa9b75b0ba47
FENRiS738 <as420738@gmail.com>
2024-05-11 14:36:12 +0530

    Impleted code so that you can directly include file url rather than its id.

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from helper__function import uploadToS3
 from pydantic import BaseModel
 
 class UploadFile(BaseModel):
-    file_id : str
+    file_url : str
 
 app = FastAPI()
 
 @app.post("/uplaod_to_s3")
 async def upload_file_to_s3(upload_file : UploadFile):
-    response = uploadToS3(upload_file.file_id)
-
+    file_id = upload_file.file_url.split("/")[-2]
+    response = uploadToS3(file_id)
+    
     return {
-        "File Id" : upload_file.file_id,
+        "File Id" : file_id,
         "Object Url" : response,
         "Created At" : datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     }
