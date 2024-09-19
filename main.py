2a869ea2c589e5d519a1b121de90e97a46c7e10f
GrowwStacks2023 <154306310+GrowwStacks2023@users.noreply.github.com>
2024-09-19 15:37:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
-from datetime import datetime
-from fastapi import FastAPI
+import os
+import gdown
+import logging
 
-from helper__function import uploadToS3
-from pydantic import BaseModel
+from connections import connectToAwsS3
 
-class UploadFile(BaseModel):
-    file_id : str
+logging.basicConfig(level=logging.INFO)
 
-app = FastAPI()
+def download_file_from_google_drive(file_id):
+    try:
+        logging.info(f"Downloading file from Google Drive with ID: {file_id}")
+        file_url = f"https://drive.google.com/uc?export=view&id={file_id}"
+        file = gdown.download(file_url, quiet=False)
+        logging.info(f"File downloaded: {file}")
+        return file
+    except Exception as e:
+        logging.error(f"Error downloading file: {e}")
+        print(f"An error occurred: {e}")
 
-@app.post("/uplaod_to_s3")
-async def upload_file_to_s3(upload_file : UploadFile):
-    response = uploadToS3(upload_file.file_id)
+def uploadToS3(file_id):
+    try:
+        logging.info(f"Uploading file to S3 with ID: {file_id}")
+        file = download_file_from_google_drive(file_id)
+        logging.info(f"File downloaded: {file}")
 
-    return {
-        "File Id" : upload_file.file_id,
-        "Object Url" : response,
-        "Created At" : datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
-    }
+        object_key = os.path.basename(file)
+        bucket_name = 'brian01bucket'
+        logging.info(f"Connecting to S3 bucket: {bucket_name}")
+        s3 = connectToAwsS3()
+        logging.info(f"S3 client connected: {s3}")
+
+        logging.info(f"Uploading file to S3: {file} -> {bucket_name}/{object_key}")
+        s3.upload_file(file, bucket_name, object_key)
+        logging.info(f"File uploaded to S3")
+
+        logging.info(f"Generating presigned URL for S3 object: {bucket_name}/{object_key}")
+        response = s3.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': object_key})
+        logging.info(f"Presigned URL generated: {response}")
+
+        logging.info(f"Removing local file: {file}")
+        os.remove(file)
+
+        return response
+    except Exception as ex:
+        logging.error(f"Error uploading file to S3: {ex}")
+        print(f"An error occurred: {ex}")
+        response = None
+        return response
